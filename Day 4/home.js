cbe1127c3301c082a382bcb182c96913b1dca635
Mohankumar2004 <110531924+Mohankumar2004@users.noreply.github.com>
2023-09-19 18:39:55 +0530

    Create home.js

---
+++ Day 4/home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './home.css';
+function Home() {
+  return (
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/about">About</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/jobs">Jobs</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/login">Login</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/signup">Signup</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+
+}
+export default Home;
+
+
+
+
+
